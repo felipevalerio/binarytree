0e0f9e31d273ef1fc67be9cb881e7c39063c8baf
Felipe <felipevalerioderamos@gmail.com>
2024-05-01 15:08:33 -0300

    a binary tree

---
+++ src/main.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+
+	int value;
+	struct Node *right;
+	struct Node *left;
+};
+typedef struct Node Node;
+
+
+void create_tree(Node **root_node) {
+
+	*root_node = NULL;
+}
+
+int main() {
+
+	
+
+	return 0;
+}
