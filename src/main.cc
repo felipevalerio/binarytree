058ded1fda14cd1c4b42dab57c7e70a90dc1d957
Felipe <felipevalerioderamos@gmail.com>
2024-05-09 23:22:54 -0300

    some corrections

--- src/main.cc
+++ src/main.cc
@@ -11,23 +11,32 @@ typedef struct Node Node;
 
 
 
-void insert(Node **root_node, int input_value) {
+void insert(Node *root_node, int input_value) {
 
-	if (*root_node == NULL) {
+	if (root_node == NULL) {
 
-		*root_node = (Node *)malloc(sizeof(Node));
-		(*root_node)->left = NULL;
-		(*root_node)->right = NULL;
-		(*root_node)->value = input_value;
+		root_node = (Node *)malloc(sizeof(Node));
+		root_node->left = NULL;
+		root_node->right = NULL;
+		root_node->value = input_value;
 
 	} else {
 
-		if (input_value < ((*root_node)->value)) {
-			insert(&((*root_node)->left), input_value);
+		if (input_value < root_node->value) {
+			insert(root_node->left, input_value);
 		} else {
-			insert(&((*root_node)->right), input_value);
+			insert(root_node->right, input_value);
 		}
 	}
+}
+
+
+void print_in_order(Node *root){
+    if(root != NULL){
+        print_in_order(root->left);
+        std::cout << " " << root->value << std::endl;
+        print_in_order(root->right);
+    }
 }
 
 void inorderTraversal(Node* root) {
@@ -41,18 +50,15 @@ void inorderTraversal(Node* root) {
 
 int main() {
 
-	Node **root = NULL;
+	Node *root = NULL;
 
 	insert(root, 10);
-	insert(root, 20);
-	insert(root, 30);
-	insert(root, 40);
-	insert(root, 50);
-	insert(root, 60);
-	insert(root, 70);
+    insert(root, 20);
+    insert(root, 30);
+    insert(root, 40);
 
 	std::cout << "Print: " << std::endl;
-	inorderTraversal(*root);
+	print_in_order(root);
 
 
 
